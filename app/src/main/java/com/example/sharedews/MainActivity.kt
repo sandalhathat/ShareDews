7eb3bd89405726877dacf4e55f2c6a8e0366e2cf
sandalhathat <sandalhathat@gmail.com>
2023-09-17 22:27:32 -0500

    added login/create account page

--- app/src/main/java/com/example/sharedews/MainActivity.kt
+++ app/src/main/java/com/example/sharedews/MainActivity.kt
@@ -3,13 +3,31 @@ package com.example.sharedews
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sharedews.ui.theme.ShareDewsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +40,94 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomePage()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ShareDewsTheme {
-        Greeting("Android")
+fun HomePage() {
+    var username by remember { mutableStateOf("")}
+    var password by remember { mutableStateOf("")}
+    var stayLoggedIn by remember {mutableStateOf(false)}
+
+
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(text = "To-Derp List")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        //username
+        TextField(
+            value = username,
+            onValueChange = { newUsername ->
+                username = newUsername
+            },
+            label = { Text("Username") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        //pw txtfld
+        TextField(
+            value = password,
+            onValueChange = { newPassword ->
+                password = newPassword
+            },
+            label = { Text("Password") },
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        //stay logged-in checkbox
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Checkbox(
+                checked = stayLoggedIn,
+                onCheckedChange = { checked ->
+                    stayLoggedIn = checked
+                },
+                modifier = Modifier.align(Alignment.CenterVertically)
+            )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = "Stay Logged-In")
+    }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        //login button
+        Button(
+            onClick = {}
+        ) {
+            Text(text = "Login")
+        }
+
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {}
+        ) {
+            Text(text = "Create Account")
+        }
+
+
     }
 }
