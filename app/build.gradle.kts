73d159bb3359d8d55fc73652a13fcdc25cd0252f
sandalhathat <sandalhathat@gmail.com>
2023-09-28 14:59:20 -0500

    Not sure if function ListItems is actually functioning as intended, but at least I've removed the errors.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
+        dataBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -65,12 +67,12 @@ dependencies {
 
     val nav_version = "2.7.2"
     //jetpack compose implementation
-    implementation("androidx.navigation:navigation-compose:$nav_version")
+    implementation("androidx.navigation:navigation-compose:2.7.3")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.09.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.09.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -79,7 +81,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.09.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.09.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
--- app/src/main/java/com/example/sharedews/ListDetailScreen.kt
+++ app/src/main/java/com/example/sharedews/ListDetailScreen.kt
@@ -1,15 +1,32 @@
 package com.example.sharedews
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -23,6 +40,30 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
+
+//import androidx.compose.foundation.clickable
+//import androidx.compose.foundation.layout.*
+//import androidx.compose.foundation.lazy.LazyColumn
+//import androidx.compose.foundation.text.BasicTextField
+//import androidx.compose.material.icons.Icons
+//import androidx.compose.material.icons.filled.Done
+//import androidx.compose.material.icons.filled.Edit
+//import androidx.compose.material.icons.filled.Star
+//import androidx.compose.material3.*
+//import androidx.compose.runtime.*
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.graphics.vector.ImageVector
+//import androidx.compose.ui.tooling.preview.Preview
+//import androidx.compose.ui.unit.dp
+//import androidx.navigation.NavController
+//import androidx.navigation.compose.rememberNavController
+//import com.google.firebase.firestore.FirebaseFirestore
+//import kotlinx.coroutines.CoroutineScope
+//import kotlinx.coroutines.Dispatchers
+//import kotlinx.coroutines.launch
+//import kotlinx.coroutines.tasks.await
+//import kotlinx.coroutines.withContext
 
 @Composable
 fun ListDetailScreen(
@@ -128,44 +169,88 @@ fun ListDetailScreen(
     }
 }
 
+//@Composable
+//fun ListItems(listItems: List<ListItem>) {
+//    LazyColumn(
+//        modifier = Modifier.fillMaxSize()
+//    ) {
+//        items(listItems.size) { index ->
+//            val item = listItems[index]
+//            ListItem(
+//                modifier = Modifier
+//                    .fillMaxWidth()
+//                    .clickable {
+//                        // Handle item click here
+//                    },
+//                text = {
+//                    Row(
+//                        verticalAlignment = Alignment.CenterVertically
+//                    ) {
+//                        CustomIcon(
+//                            icon = Icons.Default.Star,
+//                            tint = MaterialTheme.colorScheme.primary
+//                        )
+//                        Spacer(modifier = Modifier.width(16.dp))
+//                        Column {
+//                            Text(
+//                                text = item.text,
+//                                style = MaterialTheme.typography.bodySmall
+//                            )
+//                            Text(
+//                                text = "Additional Info",
+//                                style = MaterialTheme.typography.bodySmall
+//                            )
+//                        }
+//                    }
+//                }
+//            )
+//        }
+//    }
+//}
+
 @Composable
 fun ListItems(listItems: List<ListItem>) {
     LazyColumn(
         modifier = Modifier.fillMaxSize()
     ) {
-        items(listItems.size) { index ->
-            val item = listItems[index]
-            ListItem(
+        items(listItems) { item ->
+            // Destructure the item to access its properties
+            val text = item.text
+
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
                     .clickable {
                         // Handle item click here
-                    },
-                text = {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        CustomIcon(
-                            icon = Icons.Default.Star,
-                            tint = MaterialTheme.colorScheme.primary
+                    }
+                    .padding(16.dp)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    CustomIcon(
+                        icon = Icons.Default.Star,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
+                    Column {
+                        Text(
+                            text = text, // Use the extracted 'text' property
+                            style = MaterialTheme.typography.bodySmall
                         )
-                        Spacer(modifier = Modifier.width(16.dp))
-                        Column {
-                            Text(
-                                text = item.text,
-                                style = MaterialTheme.typography.bodySmall
-                            )
-                            Text(
-                                text = "Additional Info",
-                                style = MaterialTheme.typography.bodySmall
-                            )
-                        }
+                        Text(
+                            text = "Additional Info",
+                            style = MaterialTheme.typography.bodySmall
+                        )
                     }
                 }
-            )
+            }
         }
     }
 }
+
+
+
 
 data class ListItem(val text: String)
 
